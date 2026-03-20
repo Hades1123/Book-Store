44909f6c4e50b5d05043d403065b1c13bb998c29
Hades1123 <dp1.1a7.si@gmail.com>
2026-03-20 12:29:11 +0700

    refactor: remove unnecessary file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,35 @@
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 import '@/styles/global.scss';
-import { router } from '@/routes/main.route';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { StrictMode } from 'react';
 import { AuthProvider } from '@/contexts/auth.context';
+import { createBrowserRouter } from 'react-router';
+import { LoginPage } from '@/pages/auth/login.page';
+import { RegisterPage } from '@/pages/auth/register.page';
+import { OtpPage } from '@/pages/auth/otp.page';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello world</h1>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+  {
+    path: '/otp',
+    element: <OtpPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/routes/main.route.tsx
+++
@@ -1,5 +0,0 @@
-import { createBrowserRouter } from 'react-router';
-import { AuthRoute } from './auth.route';
-import { UserRoute } from './user.route';
-
-export const router = createBrowserRouter([...AuthRoute, ...UserRoute]);
